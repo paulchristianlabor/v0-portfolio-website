e775b0398b87b194686c0fc00cc672247a8368a4
paulchristianlabor <paulchristianlaborofficial@gmail.com>
2026-02-25 21:10:41 +0800

    Reverting the changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
-import { Great_Vibes } from 'next/font/google'
 
-const _greatVibes = Great_Vibes({ subsets: ["latin"], weight: "400", variable: "--font-cursive" });
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
